133db215ac8364a27f87a6620dcd2d34a9306ce2
iksadnorth <iksadnorth@gmail.com>
2025-07-26 23:08:52 +0900

    vault wrapper 프로젝트 구현

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from argparse import ArgumentParser
 from hvac import Client as hvacClient
 from datetime import datetime
-from typing import Dict, Any
+from typing import Dict, List, Any
 from jinja2 import Environment, BaseLoader, StrictUndefined
 from pathlib import Path
 
@@ -15,7 +15,11 @@ class Config:
         '.env.stage':   'stage',
         '.env.prod':    'prod',
     }
-    HIERARCHY_PHASE = [ 'prod', 'stage', 'dev', 'local' ]
+    HIERARCHY_GRAPH = {
+        'prod':     'stage',
+        'stage':    'dev',
+        'dev':      'local',
+    }
 
 class VaultEngine:
     def __init__(self, pjt_name: str, url: str, token: str):
@@ -41,7 +45,7 @@ class VaultEngine:
             return {}
 
 class EnvTemplateParser:
-    def __init__(self):
+    def __init__(self, vault: VaultEngine):
         self.env = Environment(
             loader=BaseLoader(),
             undefined=StrictUndefined,
@@ -49,18 +53,63 @@ class EnvTemplateParser:
             trim_blocks=True,
             lstrip_blocks=True
         )
+        self.vault = vault
+    
+    class Parser:
+        def __init__(self, context: Dict[str, Any], phase: str, hierarchy: List[str]) -> None:
+            self.phase = phase
+            self.context = context
+            self.hierarchy = hierarchy
+        
+        def load_inheritance(self, key):
+            try:
+                for i_phase in self.hierarchy:
+                    if i_phase not in self.context: continue
+                    if key not in self.context[i_phase]: continue
+                    if not self.context[i_phase][key]: continue
+                    
+                    return self.context[i_phase][key]
+                raise Exception
+            except Exception as e:
+                return ''
+        
+        def load(self, key):
+            try:
+                return self.context[self.phase][key]
+            except Exception as e:
+                return ''
+        
+        def load_common(self, key):
+            try:
+                return self.context['common'][key]
+            except Exception as e:
+                return ''
+    
+    def get_parser(self, phase: str, hierarchy: List[str]):
+        context = {}
+        context['common'] = self.vault.get_env('common')
+        for phase in hierarchy:
+            context[phase] = self.vault.get_env(phase)
+        
+        return EnvTemplateParser.Parser(
+            context=context,
+            phase=phase,
+            hierarchy=hierarchy
+        )
+    
+    def get_default_loader(self, key: str):
+        return "{{ "+ f"parser.load_inheritance('{key}')" +" }}"
 
 class EnvCompiler:    
     def __init__(self, vault: VaultEngine, serializer: Serializer):
         self.vault = vault
         self.serializer = serializer
-        self.parser = EnvTemplateParser()
+        self.parser = EnvTemplateParser(vault)
 
-    def commit(self, phase: str, target_env: str, template_env: str, root_dir: str = '.'):
+    def commit(self, phase: str, target_env: str, root_dir: str = '.'):
         # 파라미터 전처리
         root_path = Path(root_dir)
         file_path = root_path / target_env
-        template_path = root_path / template_env
         
         # env 파일 해석 및 저장.
         with open(file_path, "r") as f:
@@ -68,6 +117,13 @@ class EnvCompiler:
         
         env = self.serializer.unserialize(target_content)
         self.vault.write_env(phase, env)
+
+    def render(self, phase: str, template_env: str, root_dir: str = '.'):
+        # 파라미터 전처리
+        root_path = Path(root_dir)
+        template_path = root_path / template_env
+        
+        env = self.vault.get_env(phase)
 
         # 기존 파일 역직렬화
         # .env.template 파일을 근거로 존재하지 않는 키를 파악
@@ -79,7 +135,7 @@ class EnvCompiler:
         
         # 존재하지 않는 키를 새롭게 기록함
         new_table = {
-            key: "{{ load_inheritance('" +key+ "') }}"
+            key: self.parser.get_default_loader(key)
             for key in env
             if not key in template
         }
@@ -104,45 +160,8 @@ class EnvCompiler:
         # 렌더링
         with open(template_path, "r") as f:
             template = f.read()
-
-        # 파싱 문자 조회
-        context = {}
-        context['common'] = self.vault.get_env('common')
-        for phase in hierarchy:
-            context[phase] = self.vault.get_env(phase)
-        
-        def load_inheritance(key):
-            try:
-                for i_phase in hierarchy:
-                    if i_phase not in context: continue
-                    if key not in context[i_phase]: continue
-                    if not context[i_phase][key]: continue
-                    
-                    return context[i_phase][key]
-                raise Exception
-            except Exception as e:
-                return ''
-        
-        def load(key):
-            try:
-                return context[phase][key]
-            except Exception as e:
-                return ''
-        
-        def load_common(key):
-            try:
-                return context['common'][key]
-            except Exception as e:
-                return ''
-        
-        context['load_inheritance'] = load_inheritance
-        context['load'] = load
-        context['load_common'] = load_common
-        
-        # 렌더링
-        with open(template_path, "r") as f:
-            template = f.read()
-        rendered = self.parser.env.from_string(template).render(**context)
+        parser = self.parser.get_parser(phase=phase, hierarchy=hierarchy)
+        rendered = self.parser.env.from_string(template).render(parser=parser)
         
         # 파일 내용 쓰기
         with open(file_path, "w") as f:
@@ -155,57 +174,30 @@ class EnvCompiler:
         template_path = root_path / template_env
         
         # 상속 순서 설정
-        HIERARCHY_PHASE = Config.HIERARCHY_PHASE
-        idx = HIERARCHY_PHASE.index(phase)
-        hierarchy = HIERARCHY_PHASE[idx:]
-
-        # 파싱 문자 조회
-        context = {}
-        context['common'] = self.vault.get_env('common')
-        for phase in hierarchy:
-            context[phase] = self.vault.get_env(phase)
-        
-        def load_inheritance(key):
-            try:
-                for i_phase in hierarchy:
-                    if i_phase not in context: continue
-                    if key not in context[i_phase]: continue
-                    if not context[i_phase][key]: continue
-                    
-                    return context[i_phase][key]
-                raise Exception
-            except Exception as e:
-                return ''
-        
-        def load(key):
-            try:
-                return context[phase][key]
-            except Exception as e:
-                return ''
-        
-        def load_common(key):
-            try:
-                return context['common'][key]
-            except Exception as e:
-                return ''
-        
-        context['load_inheritance'] = load_inheritance
-        context['load'] = load
-        context['load_common'] = load_common
+        hierarchy = self.get_hierarchy(phase, Config.HIERARCHY_GRAPH)
+        hierarchy = list(hierarchy)
         
         # 렌더링
         with open(template_path, "r") as f:
             template = f.read()
-        rendered = self.parser.env.from_string(template).render(**context)
+        parser = self.parser.get_parser(phase=phase, hierarchy=hierarchy)
+        rendered = self.parser.env.from_string(template).render(parser=parser)
         
         # 파일 내용 쓰기
         with open(file_path, "w") as f:
             f.write(rendered)
+    
+    def get_hierarchy(self, phase: str, graph: dict):
+        yield phase
+        while phase in graph:
+            if phase == graph[phase]: return
+            yield graph[phase]
+            phase = graph[phase]
 
 class Controller:
     def __init__(self):
         parser = ArgumentParser(description="Env Management CLI")
-        parser.add_argument("command", choices=["commit", "pull", "build"])
+        parser.add_argument("command", choices=["commit", "push", "pull", "build"])
         parser.add_argument("--pjt", help="Project Name", required=True)
         
         parser.add_argument("--phase", help="Target phase")
@@ -225,7 +217,10 @@ class Controller:
         args.phase = args.phase or phase
 
         if args.command == "commit":
-            compiler.commit(args.phase, args.target, args.template, args.root)
+            compiler.commit(args.phase, args.target, args.root)
+            compiler.render(args.phase, args.template, args.root)
+        elif args.command == "push":
+            compiler.commit(args.phase, args.target, args.root)
         elif args.command == "pull":
             compiler.pull(args.phase, args.target, args.template, args.root)
         elif args.command == "build":
